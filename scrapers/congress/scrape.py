f0d8c6e7ef540273e7b4b5c29fb22779eaf3b1b5
Sarina Kaplan <sarinakaplan@Sarinas-MacBook-Pro.local>
2017-03-21 16:35:51 -0400

    added scrapers

--- scrapers/congress/scrape.py
+++ scrapers/congress/scrape.py
@@ -13,7 +13,12 @@ list_of_rows = []
 for row in table.findAll('tr')[1:-1]:
     list_of_cells = []
     for cell in row.findAll('td'):
-        list_of_cells.append(cell.text)
+        if cell.text[0] == '(':
+            party, state = cell.text.split(',')
+            list_of_cells.append(party)
+            list_of_cells.append(state)
+        else:
+        	list_of_cells.append(cell.text)
     list_of_rows.append(list_of_cells)
 
 outfile = open("women.csv", "wb")
